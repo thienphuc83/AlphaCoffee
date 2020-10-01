68d4bc35a9b67837f736e2d8be664f527250a2fb
thienphuc <phuclt1976226@gmail.com>
2020-10-01 14:46:45 +0700

    Custom lại menu

--- app/src/main/java/com/example/alphacoffee/activity/LoginActivity.java
+++ app/src/main/java/com/example/alphacoffee/activity/LoginActivity.java
@@ -3,14 +3,31 @@ package com.example.alphacoffee.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.alphacoffee.R;
 
 public class LoginActivity extends AppCompatActivity {
+    RelativeLayout rellay1, rellay2;
 
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 5000); //2000 is the timeout for the splash
     }
 }
